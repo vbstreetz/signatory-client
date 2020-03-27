b838f758db0ef13649bc44ff093b9a9509d42a83
Vb Streetz <vbstreetz@gmail.com>
2020-03-28 02:06:17 +0300

    Sat Mar 28 02:06:16 EAT 2020

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-f1f8a5af1ae80b6de431.js"
+    "url": "webpack-runtime-47064157856be4e4243f.js"
   },
   {
     "url": "styles.2b8e5fbe333634723fce.css"
@@ -45,7 +45,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e03b3b7e5945a6868946cc2d2c101e47"
+    "revision": "fa9fd1675c367ad0689eacb29749ae51"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
@@ -53,7 +53,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "34642231d0d016374b9435f46a9a41dd"
+    "revision": "dc93ed20d3694996c85ea3bdf2814d0c"
   },
   {
     "url": "manifest.webmanifest",
