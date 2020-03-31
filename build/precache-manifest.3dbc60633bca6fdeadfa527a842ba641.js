081f08365b6224d4c7800ac88c6865aba1d8fee1
Vb Streetz <vbstreetz@gmail.com>
2020-03-31 04:44:09 +0300

    Track build

---
+++ build/precache-manifest.3dbc60633bca6fdeadfa527a842ba641.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f03c310b027d459a3dd19e395dc1e5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "812cac2a6059208cae95",
+    "url": "/static/css/2.b41097e9.chunk.css"
+  },
+  {
+    "revision": "980e0ad1faef0598c026",
+    "url": "/static/css/main.910e1031.chunk.css"
+  },
+  {
+    "revision": "812cac2a6059208cae95",
+    "url": "/static/js/2.09fd1bca.chunk.js"
+  },
+  {
+    "revision": "3e76bda6332cfda2dc5a257c05255e23",
+    "url": "/static/js/2.09fd1bca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "980e0ad1faef0598c026",
+    "url": "/static/js/main.4bacd2b5.chunk.js"
+  },
+  {
+    "revision": "c2534d30d45fde370c06",
+    "url": "/static/js/runtime-main.a2f333d0.js"
+  }
+]);
--- buildx.sh
+++ buildx.sh
@@ -1,4 +1,4 @@
-PLATFORMS=linux/arm/v7,linux/arm64/v8,linux/ppc64le,linux/s390x
+PLATFORMS=linux/amd64,linux/arm/v7,linux/arm64
 
 source .env
 
